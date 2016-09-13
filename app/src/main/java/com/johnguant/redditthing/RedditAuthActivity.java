c86e979a94e8176924b95d033d20b73f71378940
Rhys Davies <rhys@johnguant.com>
2016-09-14 10:04:03 +1200

    Get one time token oauth

--- app/src/main/java/com/johnguant/redditthing/RedditAuthActivity.java
+++ app/src/main/java/com/johnguant/redditthing/RedditAuthActivity.java
@@ -1,11 +1,21 @@
 package com.johnguant.redditthing;
 
 import android.accounts.AccountAuthenticatorActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class RedditAuthActivity extends AppCompatActivity {
 
@@ -21,7 +31,37 @@ public class RedditAuthActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         WebView loginView = (WebView) findViewById(R.id.login_webview);
-        loginView.setWebViewClient(new WebViewClient());
+        loginView.setWebViewClient(new WebViewClient() {
+            boolean authComplete = false;
+            Intent result = new Intent();
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                if(url.contains("com.johnguant.redditthing://oauth2redirect") && authComplete != true){
+                    Log.d("redditThing", "response");
+                    Uri uri = Uri.parse(url);
+                    String authCode = uri.getQueryParameter("code");
+                    authComplete = true;
+                    String codeUrl = "https://www.reddit.com/api/v1/access_token";
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, codeUrl, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
         loginView.loadUrl("https://www.reddit.com/api/v1/authorize.compact?client_id=3_XCTkayxEPJuA&response_type=code&state=testing&redirect_uri=com.johnguant.redditthing://oauth2redirect&duration=permanent&scope=read%20privatemessages%20report%20identity%20livemanage%20account%20edit%20history%20flair%20creddits%20subscribe%20vote%20mysubreddits%20submit%20save%20modcontributors%20modmail%20modconfig%20modlog%20modposts%20modflair%20modothers%20modtraffic%20modwiki%20modself");
     }
 }
