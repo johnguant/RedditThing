f67128442beb179c9aed48c3cbaec60747dad375
Rhys Davies <rhys@johnguant.com>
2016-11-25 16:27:28 +1300

    Divider lines work

--- app/src/main/java/com/johnguant/redditthing/HorizontalDividerItemDecoration.java
+++
@@ -1,8 +0,0 @@
-package com.johnguant.redditthing;
-
-/**
- * Created by rhys on 4/10/16.
- */
-
-public class HorizontalDividerItemDecoration {
-}
--- app/src/main/java/com/johnguant/redditthing/MyPostRecyclerViewAdapter.java
+++ app/src/main/java/com/johnguant/redditthing/MyPostRecyclerViewAdapter.java
@@ -1,12 +1,14 @@
 package com.johnguant.redditthing;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,9 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 import java.util.Locale;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * {@link RecyclerView.Adapter} that can display a {@link Link} and makes a call to the
  * specified {@link OnListFragmentInteractionListener}.
@@ -30,7 +35,7 @@ public class MyPostRecyclerViewAdapter extends RecyclerView.Adapter<MyPostRecycl
     private List<Link> mValues;
     private final OnListFragmentInteractionListener mListener;
     LoadMoreListener more;
-    Context mContext;
+    private Context mContext;
 
     public MyPostRecyclerViewAdapter(List<Link> values, OnListFragmentInteractionListener listener, LoadMoreListener more, Context context) {
         mListener = listener;
@@ -71,6 +76,7 @@ public class MyPostRecyclerViewAdapter extends RecyclerView.Adapter<MyPostRecycl
             row2Builder.append(flairText).append(" ");
         }
         SpannableString subreddit = new SpannableString(link.getSubreddit());
+        subreddit.setSpan(new StyleSpan(Typeface.BOLD), 0, subreddit.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         row2Builder.append(subreddit).append(" ");
         SpannableString domain = new SpannableString(link.getDomain());
         row2Builder.append(domain).append(" ");
@@ -109,21 +115,18 @@ public class MyPostRecyclerViewAdapter extends RecyclerView.Adapter<MyPostRecycl
         void loadMore();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final TextView mTitleView;
-        public final TextView mRow1;
-        public final TextView mRow2;
-        public final ImageView mThumbnailView;
-        public Link mItem;
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final View mView;
+        @BindView(R.id.title) TextView mTitleView;
+        @BindView(R.id.link_row1) TextView mRow1;
+        @BindView(R.id.link_row2) TextView mRow2;
+        @BindView(R.id.preview) ImageView mThumbnailView;
+        Link mItem;
 
-        public ViewHolder(View view) {
+        ViewHolder(View view) {
             super(view);
             mView = view;
-            mTitleView = (TextView) view.findViewById(R.id.title);
-            mRow1 = (TextView) view.findViewById(R.id.link_row1);
-            mRow2 = (TextView) view.findViewById(R.id.link_row2);
-            mThumbnailView = (ImageView) view.findViewById(R.id.preview);
+            ButterKnife.bind(this, view);
         }
 
         @Override
